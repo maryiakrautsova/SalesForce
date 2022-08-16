e1d3197e2e9422b70b9561124049823e0cc6a18c
maryiakrautsova <cab.look.cab@gmail.com>
2022-08-16 19:35:29 +0300

    Added logs for test Lesson #15.

--- src/test/java/pages/ExistingAccountsPage.java
+++ src/test/java/pages/ExistingAccountsPage.java
@@ -1,9 +1,13 @@
 package pages;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ExistingAccountsPage extends BasePage {
+
+    private static final Logger LOGGER = LogManager.getLogger(ExistingAccountsPage.class.getName());
 
     private By ACCOUNT_NAME_FIELD = By.xpath("//span/slot[1]/lightning-formatted-text");
 
@@ -13,6 +17,7 @@ public class ExistingAccountsPage extends BasePage {
 
 
     public String getAccountName() {
+        LOGGER.debug(String.format("Attempt to get existing account name: %s.", ACCOUNT_NAME_FIELD));
         return driver.findElement(ACCOUNT_NAME_FIELD).getText();
     }
 }
--- src/test/java/tests/CreateAccountTest.java
+++ src/test/java/tests/CreateAccountTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import models.NewAccountModel;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AccountsPage;
@@ -10,23 +12,39 @@ import pages.NewAccountModelPage;
 import testdata.PrepareNewAccountData;
 
 public class CreateAccountTest extends BaseTest {
+
+    private static final Logger LOGGER = LogManager.getLogger(CreateAccountTest.class.getName());
+
     @Test
     public void createAccountTest() {
         LoginPage loginPage = new LoginPage(driver);
+        LOGGER.info(String.format("Page %s is initialized.", LoginPage.class.getName()));
+        LOGGER.info(String.format("Open %s page.", LoginPage.class.getName()));
+        LOGGER.info("Attempt to log in into the system.");
         loginPage.loginToSalesforce();
         AccountsPage accountsPage = new AccountsPage(driver);
+        LOGGER.info(String.format("Page %s is initialized.", AccountsPage.class.getName()));
+
+        LOGGER.info(String.format("Attempt to get prepared account data."));
         NewAccountModel accountData = PrepareNewAccountData.getValidData();
 
+        LOGGER.info(String.format("Open %s page.", AccountsPage.class.getName()));
+        LOGGER.info("Proceed with opening page to create a new account and opening an account model.");
         NewAccountModelPage newAccountModelPage = accountsPage.
                 openAccountsPage().
                 openNewAccountModel();
 
+        LOGGER.info(String.format("Account form on %s is filled in.", AccountsPage.class.getName()));
         newAccountModelPage.fillInAccountForm(accountData);
         String filledInAccountName = accountData.getAccountName();
+        LOGGER.info(String.format("Attempt to get created account name: %s.", filledInAccountName));
         newAccountModelPage.clickSaveNewAccount();
 
         ExistingAccountsPage existingAccountsPage = new ExistingAccountsPage(driver);
+        LOGGER.info(String.format("Page %s is initialized.", ExistingAccountsPage.class.getName()));
         String actualAccountName = existingAccountsPage.getAccountName();
+        LOGGER.info(String.format("Attempt to get account name: %s.", actualAccountName));
+        LOGGER.info("Check whether existing account name equals to account name from account form.");
         Assert.assertEquals(actualAccountName, filledInAccountName);
     }
 }
